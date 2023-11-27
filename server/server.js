dceb790aa6d128df941e8473be350621ad2551a8
CharlesYorke1019 <106764082+CharlesYorke1019@users.noreply.github.com>
2023-11-27 11:18:32 -0500

    Updating server.js (Mon Nov 27th 11:15 A.M.)
    Uploading the updates for server.js:
    
    - changed how Redis stores the data
    - only setting data under 'CONV' and 'CONV_AS_OF_${current time}'
    - helps because I wont have to technically flush the data after 24 hours (only need to keep data for 24 hrs), but instead it essentially just takes its place for the next dat
    
    For example:
    - tuesdays conversion rate at 11:50 A.M. will take over the conversion rate of mondays conversion rate at 11:50 A.M. (or 'CONV_AS_OF_${current time}')

--- server/server.js
+++ server/server.js
@@ -36,17 +36,9 @@ async function fetchApiData() {
     return response.data;
 }
 
-const storeInfo = (info1, info2, info3, time, date) => {
-
-    redisClient.set(`ETH`, info1);
-    redisClient.set(`EUR`, info2);
+const storeInfo = (info1, info2, info3, time) => {
     redisClient.set(`CONV`, info3)
-
-    // store new data with Redis using the experation option (since we only need to store the data for 24 hours at a time) //
-    // redisClient.setEx(`ETH`, 86400, info1);
-    // redisClient.setEx(`EUR`, 86400, info2);
-    // redisClient.setEx(`CONV`, 86400, info3);
-
+    redisClient.set(`CONV_AS_OF_${time}`, info3);
 }
 
 // how to retrieve values with Redis //
@@ -54,20 +46,13 @@ const retrieveInfo = () => {
     return redisClient.get(`CONV`);
 }
 
-let dateHolder = new Date();
-
 // when the app is first ran, fetchApiData() is called so that we can populate redis with data (could just wait until the setInterval() call below hits, but then redis will not be populated for 60 seconds)
 fetchApiData().then((info) => {
     let conversion = info.EUR / info.ETH
-    storeInfo(info.ETH, info.EUR, conversion);  
+    let currentTime = new Date().getHours().toString() + ":" + new Date().getMinutes().toString();
+    storeInfo(info.ETH, info.EUR, conversion, currentTime);  
 
-    // index = new Date().getHours().toString() + new Date().getMinutes().toString();
-    // index = new Date().getHours().toString() + new Date().getMinutes().toString() - 1600;
-
-    index = new Date().getHours();
-    // set index to what's inside console.log() below when all times of day are set up on front end
-    // console.log(new Date().getHours() * 60  + new Date().getMinutes());
-
+    index = new Date().getHours() * 60  + new Date().getMinutes();
     globalData[index] = conversion;
     index++;
 });
@@ -76,11 +61,9 @@ fetchApiData().then((info) => {
 setInterval(() => {
     fetchApiData().then((info) => {
         let conversion = info.EUR / info.ETH
-        // let date = dateHolder.getDate();
-        // let currentTime = new Date().getHours().toString() + ":" + new Date().getMinutes().toString();
-        storeInfo(info.ETH, info.EUR, conversion);
-        
-        if (index >= 23) {
+        let currentTime = new Date().getHours().toString() + ":" + new Date().getMinutes().toString();
+        storeInfo(info.ETH, info.EUR, conversion, currentTime);
+        if (index >= 1439) {
             globalData = [conversion];
             index = 1;
         } else {
@@ -88,29 +71,23 @@ setInterval(() => {
             index++;
         }
     });
-}, 10000)
+}, 60000)
 
 // listening for users connecting to the server
 io.on('connection', (socket) => {
     console.log('connection')
-    // console.log(globalData);
 
     // when a user connects, we grab the current time and call retrieveInfo() so that we can emit the desired info back to the user (could just wait for the setInterval() call below but users wouldn't get data for 60 seconds on connecting)
-    let currentTime = new Date().getHours().toString() + ":" + new Date().getMinutes().toString();
     retrieveInfo().then((info) => {
         io.emit('sendingBackData', info, index, globalData);
     })
 
     // we grab the current time, we then retrieve the info from Redis and emit it back to the users, we call this in setInterval so that every minute the data is checked updated and sent back to the user
     setInterval(() => {
-
-        let currentTime = new Date().getHours().toString() + ":" + new Date().getMinutes().toString();
         retrieveInfo().then((info) => {
             io.emit('sendingBackData', info, index, globalData);
         })
-
-
-    }, 10000)    
+    }, 60000)    
 
 })
 
