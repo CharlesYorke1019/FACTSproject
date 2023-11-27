c53e35dd1811fd99a849e60d7279bee6e43f982e
CharlesYorke1019 <106764082+CharlesYorke1019@users.noreply.github.com>
2023-11-27 15:14:43 -0500

    Uploading App.vue Updates (Mon Nov 27th 2:20)
    Updates: 
    
    - just cleaned up App.vue 
    - deleted unnecessary elements

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,14 @@ import BarChart from './components/BarChart.vue';
 
 <template>
   <header>
-
   
   </header>
 
-  <div id="testing">
+  <div id="divContainer">
 
     <h1>{{ date }}</h1>
 
     <bar-chart :conversion="conversion" :socket="sock" />
-
 
   </div>
 
@@ -21,35 +19,18 @@ import BarChart from './components/BarChart.vue';
 
 <script>
 import io from 'socket.io-client'
-import BarChart from './components/BarChart.vue';
 const socket = io('http://localhost:5000', { transports: ['websocket', 'polling', 'flashsocket'] });
 
-import { Line } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement, Chart} from 'chart.js'
-  
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement)
-
 export default {
-  components: { Line },
   name: 'App',
   data() {
     return {
-      message: [],
-      date: '',
-      eth: '',
       conversion: 0,
+      date: '',
       sock: socket
     }
   },
-  methods: {
-    test() {
-      socket.emit('hello');
-    }
-  },
   created() {
-    // socket.on('sendingBackData', (info) => {
-    //   this.conversion = info;
-    // })
     this.date = new Date().toDateString();
   }
 }
@@ -59,14 +40,8 @@ export default {
 
 <style scoped>
 
-body {
-  background-color: white;
-}
-
-#testing {
+#divContainer {
   width: 1250px
 }
-
-
 
 </style>
