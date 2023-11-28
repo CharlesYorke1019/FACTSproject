1a9d997774e860606539c7a45a00cf14a36c4f25
CharlesYorke1019 <106764082+CharlesYorke1019@users.noreply.github.com>
2023-11-28 10:09:51 -0500

    uploading views folder

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div id="mainContainer">
+    <h1 id="applicationHeader">Welcome To The ETH/EUR Tracking Application!</h1>
+    <div style="width: 800px; border-color: white; border-width: 1;"><canvas id="acquisitions"></canvas></div>
+  </div>
+</template>
+
+<style scoped>
+
+  #applicationHeader {
+    position: absolute;
+    font-family: 'Copperplate';
+    left: '10%';
+  }
+
+</style>
